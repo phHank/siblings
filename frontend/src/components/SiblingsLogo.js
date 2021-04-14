63fbb5392b4e4e5d34a2dfbaafa2b3c21119083e
Philip Hankey <hankey.ph@gmail.com>
2021-04-14 19:32:56 +0100

    Refactor index.html files to include local static

--- frontend/src/components/SiblingsLogo.js
+++ frontend/src/components/SiblingsLogo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SiblingsLogo = ({height}) => (
     <div style={{position: 'relative', overflow: 'auto', height: height}}>
         <img 
-            src='https://siblings.tametraffic.com/api/web/uploads/companypicture/411.png?id=58tvowkza2ln8333xu9gj'
+            src='/static/logo.png'
             alt='Siblings together makes sense - hugging giraffes logo'
             className='mh-100 mw-100'
         />
